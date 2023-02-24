a2e30c3bcc9043ed5f58e41639354bdd800c5e3b
surelle-ha <0110harold@gmail.com>
2023-02-25 05:00:28 +0800

    autoCommit: What lazy dev do.

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -4,7 +4,7 @@ const announcementController = require('../controllers/announcementController')
 
 router.post('/test', announcementController.test)
 router.post('/addAnnouncement', announcementController.addAnnouncement)
-router.get('/removeAnnouncement', announcementController.removeAnnouncement)
+router.delete('/removeAnnouncement', announcementController.removeAnnouncement)
 router.get('/acknowledgeAnnouncement', announcementController.acknowledgeAnnouncement)
 
 module.exports = router
